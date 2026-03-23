b52b97b1e67049d5ed1086e4e67c1aa4877d2f69
Tiago Su <tiagosu258@gmail.com>
2026-03-23 12:05:05 +0000

    Documenting the adapter.h and maxFlow.h

--- include/maxFlow.h
+++ include/maxFlow.h
@@ -3,14 +3,44 @@
 
 #include "Graph.h"
 
+/**
+ * @class MaxFlow
+ * @brief Class that executes EdmondsKarp maximum flow algorithm
+ */
 class MaxFlow {
   public:
+	/**
+	* @brief Method that executes the EdmondsKarp maximum flow algorithm
+	* @param g Graph to be applied the algorithm
+	* @param source The source node
+	* @param target The sink node
+	*/
     void edmondsKarp(Graph<int>* g, int source, int target);
 
   private:
-    void testAndVisit(std::queue<Vertex<int>*>& q, Edge<int>* e, Vertex<int>* w, double residual);
+	/**
+	* @brief Method that finds an augmenting path from source to sink. The found path is stored in node's path
+	* @param g Graph to find an augmenting path
+	* @param s Source node
+	* @param t Sink node
+	* @return Return true if found an augmenting path from source to sink
+	*/
     bool findAugmentingPath(Graph<int>* g, Vertex<int>* s, Vertex<int>* t);
+
+	/**
+	* @brief Method that finds the minimum residual value from the augmenting path found
+	* @param s Source node
+	* @param t Sink node
+	* @return The minimum residual value found in the augmenting path
+	*/
     double findMinResidualAlongPath(Vertex<int>* s, Vertex<int>* t);
+
+	/**
+	* @brief Method that updates the residual graph
+	* @param s Source node
+	* @param t Sink node
+	* @param f Flow
+	*/
     void augmentFlowAlongPath(Vertex<int>* s, Vertex<int>* t, double f);
 };
 
